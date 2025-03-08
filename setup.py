b278657b5072f441a4610d6ff92a7aeaba3c825d
Mahammad Ehtaramullah <mahammadehtaramullah@gmail.com>
2025-03-09 03:17:05 +0530

    folder added

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Generative AI Project',
     version= '0.0.0',
-    author= 'Md Ehtaramullah',
-    author_email= 'mahammadehtaramullah@gmail.com',
+    author= 'Bappy',
+    author_email= 'boktiar@ineuron.ai',
     packages= find_packages(),
     install_requires = []
 
